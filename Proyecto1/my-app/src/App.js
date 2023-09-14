de9050af9d0dae8b96aae761c40577b5dde1ff9a
Brayan-PM <78266710+BrayanPradoMarroquin@users.noreply.github.com>
2023-09-13 23:45:41 -0600

    Interfaz principal

--- Proyecto1/my-app/src/App.js
+++ Proyecto1/my-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import docker from './img/docker.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <img src={docker} alt="docker" width="60" height="60" className="d-inline-block align-text-top" />
+          <Navbar.Brand><h1>Modulos de Kernel</h1></Navbar.Brand>
+
+          <Nav className="me-auto">
+            <Nav.Link href="#home"><h2> Tiempo Real</h2></Nav.Link>
+            <Nav.Link href="#features"><h2>Historial</h2></Nav.Link>
+            <Nav.Link><h5>BHEPM - 201801369</h5></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
+
+    
   );
 }
 
